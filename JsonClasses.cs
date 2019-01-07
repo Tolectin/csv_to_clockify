de1062084fb7f9cc7a6046949ccbfb9aa945a27e
Tolectin <46463680+Tolectin@users.noreply.github.com>
2019-01-07 15:05:18 -0800

    Initial commit

---
+++ JsonClasses.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToClockify
+{
+    public class JsonClasses
+    {
+    }
+
+    public class HourlyRate
+    {
+        public string amount { get; set; }
+        public string currency { get; set; }
+    }
+
+    public class Membership
+    {
+        public HourlyRate hourlyRate { get; set; }
+        public string membershipStatus { get; set; }
+        public string membershipType { get; set; }
+        public string target { get; set; }
+        public string userId { get; set; }
+    }
+
+    public class Round
+    {
+        public string minutes { get; set; }
+        public string round { get; set; }
+    }
+
+    public class WorkspaceSettings
+    {
+        public string canSeeTimeSheet { get; set; }
+        public string defaultBillableProjects { get; set; }
+        public string forceDescription { get; set; }
+        public string forceProjects { get; set; }
+        public string forceTags { get; set; }
+        public string forceTasks { get; set; }
+        public string lockTimeEntries { get; set; }
+        public string onlyAdminsCreateProject { get; set; }
+        public string onlyAdminsSeeAllTimeEntries { get; set; }
+        public string onlyAdminsSeeBillableRates { get; set; }
+        public string onlyAdminsSeeDashboard { get; set; }
+        public string onlyAdminsSeePublicProjectsEntries { get; set; }
+        public string projectFavorites { get; set; }
+        public string projectPickerSpecialFilter { get; set; }
+        public Round round { get; set; }
+        public string timeRoundingInReports { get; set; }
+    }
+
+    public class Workspace
+    {
+        public HourlyRate hourlyRate { get; set; }
+        public string id { get; set; }
+        public string imageUrl { get; set; }
+        public List<Membership> memberships { get; set; }
+        public string name { get; set; }
+        public WorkspaceSettings workspaceSettings { get; set; }
+    }
+
+    public class Client
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string workspaceId { get; set; }
+    }
+
+    public class Estimate
+    {
+        public string estimate { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Task
+    {
+        public string assigneeId { get; set; }
+        public string estimate { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string projectId { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Project
+    {
+        public string archived { get; set; }
+        public string billable { get; set; }
+        public Client client { get; set; }
+        public string clientId { get; set; }
+        public string color { get; set; }
+        public Estimate estimate { get; set; }
+        public HourlyRate hourlyRate { get; set; }
+        public string id { get; set; }
+        public List<Membership> memberships { get; set; }
+        public string name { get; set; }
+        public string @public { get; set; }
+        public List<Task> tasks { get; set; }
+        public string workspaceId { get; set; }
+    }
+    public class TimeEntry
+    {
+        public string start { get; set; }
+        public string billable { get; set; }
+        public string description { get; set; }
+        public string projectId { get; set; }
+        public string taskId { get; set; }
+        public string end { get; set; }
+        public List<string> tagIds { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToClockify
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();        
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+                Console.WriteLine("Usage: CsvToClockify <api_key> <csv_input_file>");
+
+            RunAsync(args[0], args[1]).GetAwaiter().GetResult();
+        }
+
+        static async Task<int> RunAsync(string api_key, string inputFile)
+        {
+            Dictionary<string, string> workspaceMap = new Dictionary<string, string>();
+            Dictionary<string, string> projectMap = new Dictionary<string, string>();
+
+            client.BaseAddress = new Uri("https://api.clockify.me/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
+
+            // Get all the workspaces
+            var response = await client.GetAsync("workspaces/");
+            if (response.IsSuccessStatusCode)
+            {
+                List<Workspace> workspaces = await response.Content.ReadAsAsync<List<Workspace>>();
+                foreach(var workspace in workspaces)
+                    workspaceMap.Add(workspace.name, workspace.id);
+            }
+
+            // Get all the projects
+            foreach (var workspaceKvp in workspaceMap)
+            {
+                response = await client.GetAsync($"workspaces/{workspaceKvp.Value}/projects/");
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Project> projects = await response.Content.ReadAsAsync<List<Project>>();
+                    foreach (var project in projects)
+                        projectMap.Add(workspaceKvp.Key + project.client?.name + project.name, project.id);
+                }
+            }
+
+            // Read the input file            
+            using (TextFieldParser csvParser = new TextFieldParser(inputFile))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                csvParser.ReadLine();
+
+                // Start creating tasks
+                while (!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvParser.ReadFields();
+
+                    string test = projectMap.Keys.ElementAt(3);
+                    string test2 = $"{fields[0]}{fields[1]}{fields[2]}";
+
+                    // Create a time entry
+                    TimeEntry timeEntry = new TimeEntry();
+                    timeEntry.projectId = projectMap[$"{fields[0]}{fields[1]}{fields[2]}"];
+                    timeEntry.start = Convert.ToDateTime($"{fields[4]} {fields[5]}").ToUniversalTime()
+                         .ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'");
+                    timeEntry.end = Convert.ToDateTime($"{fields[6]} {fields[7]}").ToUniversalTime()
+                         .ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'");
+                    timeEntry.billable = "true";
+                    timeEntry.description = fields[3];
+                    //timeEntry.taskId = "";
+                    //timeEntry.tagIds = new List<string>{ "" };
+
+                    response = await client.PostAsJsonAsync($"workspaces/{workspaceMap[fields[0]]}/timeEntries/", timeEntry);
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+
+            return 1;
+        }
+    }
+}
